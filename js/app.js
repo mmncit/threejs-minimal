0786db7f7095e91a2f3e2aefb20602a48f0b26c3
mamun <mamun.rashid@seequent.com>
2020-02-18 15:35:06 -0700

    add light to the scene

--- js/app.js
+++ js/app.js
@@ -1,26 +1,34 @@
 var scene = new THREE.Scene();
+
+{
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
+
 var camera = new THREE.PerspectiveCamera( 45, 
     window.innerHeight / window.innerHeight, 
     0.1,
     1000
 );
-            var renderer = new THREE.WebGLRenderer();
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            document.body.appendChild(renderer.domElement);
 
-            var geometry = new THREE.BoxGeometry();
-            var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-            var cube = new THREE.Mesh(geometry, material);
-            scene.add(cube);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-            camera.position.z = 5;
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshPhongMaterial({color: 0x44aa88});  // greenish blue
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-            function animate(){
-                requestAnimationFrame(animate);
+camera.position.z = 5;
 
-                //cube.rotation.x += 0.01;
-                cube.rotation.y += 0.01;
-
-                renderer.render(scene, camera);
-            }
-            animate();
+function animate(){
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+animate();
