92e8e5685e305789308cbf2c26fd2a8ee72ef05d
mamun <mamun.rashid@seequent.com>
2020-03-02 11:55:35 -0700

    set movement

--- js/app.js
+++ js/app.js
@@ -1,21 +1,46 @@
 var scene, camera, renderer, mesh;
+var keyboard = {};
+var player = {height: 1.8};
+const width = 1280;
+const height = 720;
+
+function CustomSinCurve( scale ) {
+
+	THREE.Curve.call( this );
+
+	this.scale = ( scale === undefined ) ? 1 : scale;
+
+}
+
+CustomSinCurve.prototype = Object.create( THREE.Curve.prototype );
+CustomSinCurve.prototype.constructor = CustomSinCurve;
+
+CustomSinCurve.prototype.getPoint = function ( t ) {
+
+	var tx = t * 3 - 1.5;
+	var ty = Math.sin( 2 * Math.PI * t );
+	var tz = 0;
+
+	return new THREE.Vector3( tx, ty, tz ).multiplyScalar( this.scale );
+
+};
 
 function init(){
 	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera(90, 1280/720, 0.1, 1000);
+	camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
 
 	mesh = new THREE.Mesh(
-		new THREE.BoxGeometry(1, 1, 1),
+		new THREE.TubeGeometry( new CustomSinCurve(20), 20, 2, 8, false),
 		new THREE.MeshBasicMaterial({color: 0x0ff9999, wireframe: true})
 	);
 
 	scene.add(mesh);
 
-	camera.position.set(0, 0, 5);
-	camera.lookAt(new THREE.Vector3(0, 0, 0));
+	camera.position.set(0, player.height, 100);
+	camera.lookAt(new THREE.Vector3(0, player.height, 0));
 
 	renderer = new THREE.WebGLRenderer();
-	renderer.setSize(1280, 720);
+	renderer.setSize(width, height);
 	document.body.appendChild(renderer.domElement);
 
     animate();
@@ -27,6 +52,14 @@ function animate(){
 
 	mesh.rotation.x += 0.01;
 	mesh.rotation.y += 0.02;
+
+	if(keyboard[37]){ // left arrow key
+		camera.rotation.y -= Math.PI * 0.01;
+	}
+	if(keyboard[39]){ // right arrow key
+		camera.rotation.y += Math.PI * 0.01;
+	}
+
 
 	renderer.render(scene, camera);
 }
