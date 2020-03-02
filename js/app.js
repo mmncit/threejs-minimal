ce19f324dfcaca8b68173d827f0d7dedee0ff224
mamun <mamun.rashid@seequent.com>
2020-03-02 11:25:35 -0700

    draw tube

--- js/app.js
+++ js/app.js
@@ -1,36 +1,50 @@
+
+
 var scene = new THREE.Scene();
-
-{
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.add(light);
-  }
-
-var camera = new THREE.PerspectiveCamera( 45, 
-    window.innerHeight / window.innerHeight, 
-    0.1,
-    1000
-);
+var camera = new THREE.PerspectiveCamera( 40, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const loader = new THREE.TextureLoader();
-const material = new THREE.MeshBasicMaterial({map: loader.load('../resources/wall.jpg'),
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+function CustomSinCurve( scale ) {
 
-camera.position.z = 5;
+	THREE.Curve.call( this );
 
-function animate(){
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+	this.scale = ( scale === undefined ) ? 1 : scale;
+
 }
+
+CustomSinCurve.prototype = Object.create( THREE.Curve.prototype );
+CustomSinCurve.prototype.constructor = CustomSinCurve;
+
+CustomSinCurve.prototype.getPoint = function ( t ) {
+
+	var tx = t * 3 - 1.5;
+	var ty = Math.sin( 2 * Math.PI * t );
+	var tz = 0;
+
+	return new THREE.Vector3( tx, ty, tz ).multiplyScalar( this.scale );
+
+};
+
+var path = new CustomSinCurve( 10 );
+var geometry = new THREE.TubeGeometry( path, 20, 2, 8, false);
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, side: THREE.BackSide, wireframe: true});
+
+var mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+
+
+camera.position.z = 50;
+
+var animate = function () {
+	requestAnimationFrame( animate );
+
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+};
 animate();
