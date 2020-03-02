109e1d8b9cfe37380627eb63a10a8abd5a31a077
mamun <mamun.rashid@seequent.com>
2020-03-02 11:37:29 -0700

    draw mesh geometry

--- js/app.js
+++ js/app.js
@@ -1,50 +1,34 @@
+var scene, camera, renderer, mesh;
 
+function init(){
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(90, 1280/720, 0.1, 1000);
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 40, window.innerWidth/window.innerHeight, 0.1, 1000 );
+	mesh = new THREE.Mesh(
+		new THREE.BoxGeometry(1, 1, 1),
+		new THREE.MeshBasicMaterial({color: 0x0ff9999, wireframe: true})
+	);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+	scene.add(mesh);
 
-function CustomSinCurve( scale ) {
+	camera.position.set(0, 0, 5);
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-	THREE.Curve.call( this );
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(1280, 720);
+	document.body.appendChild(renderer.domElement);
 
-	this.scale = ( scale === undefined ) ? 1 : scale;
-
+    animate();
 }
 
-CustomSinCurve.prototype = Object.create( THREE.Curve.prototype );
-CustomSinCurve.prototype.constructor = CustomSinCurve;
+function animate(){
 
-CustomSinCurve.prototype.getPoint = function ( t ) {
-
-	var tx = t * 3 - 1.5;
-	var ty = Math.sin( 2 * Math.PI * t );
-	var tz = 0;
-
-	return new THREE.Vector3( tx, ty, tz ).multiplyScalar( this.scale );
-
-};
-
-var path = new CustomSinCurve( 10 );
-var geometry = new THREE.TubeGeometry( path, 20, 2, 8, false);
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, side: THREE.BackSide, wireframe: true});
-
-var mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
-
-
-
-camera.position.z = 50;
-
-var animate = function () {
-	requestAnimationFrame( animate );
+	requestAnimationFrame(animate);
 
 	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.01;
+	mesh.rotation.y += 0.02;
 
-	renderer.render( scene, camera );
-};
-animate();
+	renderer.render(scene, camera);
+}
+
+window.onload = init;
